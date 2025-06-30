24919d045679e80ef2d5ac81e6845386b7f71ac6
Gopal Sharma <Gopalshar852@gmail.com>
2025-06-30 14:00:08 +0530

    Dsa

--- Lec_3/Lec_4/pattern_11.cpp
+++ Lec_3/Lec_4/pattern_11.cpp
@@ -1,14 +1,32 @@
+// #include <iostream>
+// using namespace std;
+// int main (){
+//       int n;
+//       cout<<("Enter the value of n: ");
+//       cin>>n;
+//       int x=65;
+//       for(int i=1;i<=n;i++){
+//             for(int j = 1;j<=i;j++){
+//                   cout<<(char(x)) <<" ";
+//                   x++;
+//             }
+//             cout<<"\n";
+//       }
+
+// }
 #include <iostream>
 using namespace std;
 int main (){
       int n;
       cout<<("Enter the value of n: ");
       cin>>n;
-      int x=65;
       for(int i=1;i<=n;i++){
-            for(int j = 1;j<=i;j++){
-                  cout<<(char(x)) <<" ";
-                  x++;
+            for(int j = 1;j<=i-1;j++){
+                  cout<<" ";
+                  for(int k = 1; k=n;k++){
+                        cout<<k;
+                  }
+            
             }
             cout<<"\n";
       }
