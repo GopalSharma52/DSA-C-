039c4d1a2fdaa2b0d23106772c9bd6c25ed91061
Gopal Sharma <Gopalshar852@gmail.com>
2025-08-22 22:00:58 +0530

    DSA

--- BinarySearch/Pivot.cpp
+++ BinarySearch/Pivot.cpp
@@ -1,18 +1,22 @@
 #include<iostream>
 using namespace std;
-int main(){
-      int arr [5] = {7,9,1,2,3 };
+int getPivot(int arr[],int size){
       int start = 0;
-      int end = sizeof(arr)/4-1;
-      int mid = start + ( end - start )/2;
+      int end = size - 1;
+      int mid = start + (end - start)/2;
       while(start<end){
-            if(arr[mid]<=arr[0]){
-                  start  = start + 1;
+            if(arr[mid]>=arr[0]){
+                  start = mid + 1;
             }
             else{
                   end = mid;
             }
-            mid = start + ( end - start )/2;
+            mid = start + (end - start)/2;
       }
-      cout<<"Pivot element is : "<<start;
+      return start;
+}
+int main(){
+    int arr[7]= {9,8,7,1,2,3,4};
+    int pivot = getPivot(arr,7);
+    cout<<"Index of Pivot element is: "<<pivot;
 }
